c59d764bdf47f3c289815b8f82d4ecf2d9b398b2
jeongchanmin <jjw421412@naver.com>
2024-07-16 21:31:17 +0900

    :recycle: 유사도 평가 함수 수정

--- src/service/nlp/similarity.py
+++ src/service/nlp/similarity.py
@@ -1,14 +1,11 @@
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def calculate_similarity(matrix, vectorizer, query):
-    if query is None:
-        query = ''
-    query = vectorizer.transform([query])
+def cal_similarity(matrix, query):
     similarities = cosine_similarity(matrix, query)
     return similarities
 
 
-def max_similarity(df, similarities, n=1):
-    indices = similarities.argsort(axis=0)[::-1].flatten()
-    return [df.iloc[idx] for idx in indices[:n]]
+def max_similarity(similarities, n=1):
+    indices = similarities.flatten().argsort()[::-1]
+    return indices[:n]
