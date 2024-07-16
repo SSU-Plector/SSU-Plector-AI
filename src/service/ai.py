79d7392cbd0bed928dc0be322767d47df017599c
jeongchanmin <jjw421412@naver.com>
2024-07-16 21:30:11 +0900

    :sparkles: [Feat] 단어 기반 유사도 + 문장 의미 기반 유사도 방식으로 유사도 평가 방식 변경

--- src/service/ai.py
+++ src/service/ai.py
@@ -1,8 +1,10 @@
 from flask import abort
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 from src.enum.part import Part
 from src.service.database import developer_part_eq
-from src.service.nlp.feature_engineering import tfidf_feature_engineering
-from src.service.nlp.similarity import calculate_similarity, max_similarity
+from src.service.nlp.embedding import get_bert_embeddings
+from src.service.nlp.similarity import max_similarity, cal_similarity
 
 
 # 개발자 매칭 함수
@@ -18,15 +20,24 @@ def developer_matching(data):
     df = developer_part_eq(part_enum)
     if df.empty:
         return []
+    if query is None:
+        query = ''
 
-    tfidf_matrix, vectorizer = tfidf_feature_engineering(df, 'short_intro')
-    similarities = calculate_similarity(tfidf_matrix, vectorizer, query)
-    recommended_developers = max_similarity(df, similarities, 5)
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(df['short_intro'].tolist())
+    query_tfidf = tfidf_vectorizer.transform([query])
+    tfidf_similarities = cal_similarity(tfidf_matrix, query_tfidf)
 
-    for dev in recommended_developers:
-        dev['developer_id'] = int(dev['developer_id'])
+    developer_embeddings = get_bert_embeddings(df['short_intro'].tolist())
+    query_embedding = get_bert_embeddings([query])
+    embedding_similarities = cal_similarity(developer_embeddings, query_embedding)
 
-    return [
-        {'developer_id': dev['developer_id']}
-        for dev in recommended_developers
-    ]
+    hybrid_similarities = 0.5 * tfidf_similarities + 0.5 * embedding_similarities
+    recommended_indices = max_similarity(hybrid_similarities, 5)
+
+    result_df = df.iloc[recommended_indices].copy()
+    result_df['developer_id'] = result_df['developer_id'].astype(int)
+
+    return {
+        'developers': [{'developer_id': dev_id} for dev_id in result_df['developer_id']]
+    }
