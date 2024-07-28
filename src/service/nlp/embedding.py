9a15abed3129fc423e1375011983929717590eb3
88dldl <chltnrgml0999@gmail.com>
2024-07-28 19:16:17 +0900

    [Refactor] torch import 방법 변경

--- src/service/nlp/embedding.py
+++ src/service/nlp/embedding.py
@@ -1,22 +1,9 @@
-import os
-import sys
-
-pytorch_path = os.getenv('PYTORCHPATH')
-
-if pytorch_path:
-    if pytorch_path not in sys.path:
-        sys.path.append(pytorch_path)
-
-# 이제 torch 모듈을 임포트
-import torch
-print(f"torch 패키지 버전: {torch.__version__}")
-
 from transformers import BertTokenizer, BertModel
+import torch
 
-# BERT 모델과 토크나이저 로드
+# BERT 모델과 토크나이저를 로드합니다.
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased')
-
 
 def get_bert_embeddings(text_list):
     inputs = tokenizer(text_list, return_tensors='pt', padding=True, truncation=True, max_length=512)
